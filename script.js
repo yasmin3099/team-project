98760bba76fd0024181c9f1aaceebdffb3440bd8
Mohamed Abdelwahab <mohamedabdelwahabali5@gmail.com>
2025-01-20 16:58:35 +0200

    multiply function is done successfully

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var x = 1;
 var y = 2;
-function add (){
-var t = x+y;
-console.log(t);
+
+function add() {
+    var t = x + y;
+    console.log(t);
 }
 
-function spare (){
-    console.log(x-y);
+function spare() {
+    console.log(x - y);
 }
+
+
+/// mohamed abdelwahab
+function multiply(parameter1, parameter2) {
+    return parameter1 * parameter2;
+}
+
+console.log(multiply(4, 5));
