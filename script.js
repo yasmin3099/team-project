d331df197008b3fa42832cd0cffaba8d634d655f
Omar Araby <omararaby789@gmail.com>
2025-01-20 17:36:19 +0200

    script after pull request

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ var x = 1;
 var y = 2;
 
 function add() {
-    var t = x + y;
-    console.log(t);
+  var t = x + y;
+  console.log(t);
 }
 
 function spare() {
-    console.log(x - y);
+  console.log(x - y);
 }
-
 
 /// mohamed abdelwahab
 function multiply(parameter1, parameter2) {
-    return parameter1 * parameter2;
+  return parameter1 * parameter2;
 }
 
 console.log(multiply(4, 5));
