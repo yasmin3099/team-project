e2e185cd4b990eedf79494bd241c303cfafd5b55
Omar Araby <omararaby789@gmail.com>
2025-01-20 17:33:47 +0200

    Division logic

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var x = 1;
 var y = 2;
-function add (){
-var t = x+y;
-console.log(t);
+function add() {
+  var t = x + y;
+  console.log(t);
 }
 
-function spare (){
-    console.log(x-y);
+function spare() {
+  console.log(x - y);
 }
+
+// division
+function division(p1, p2) {
+  if (p2 != 0) {
+    return p1 / p2;
+  }
+}
+
+console.log(division(12 / 3));
